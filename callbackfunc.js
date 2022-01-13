512c2061c5b24bc06e3a3c322ea69d0598e30306
Abhinav Choudhary <89508503+abhinavchoudhary12@users.noreply.github.com>
2022-01-13 17:40:27 +0530

    call back function

---
+++ callbackfunc.js
@@ -0,0 +1,31 @@
+function add(num1,num2,addition)
+{
+var a=num1+num2
+console.log("addition of two numbers are : "+a)
+}
+function output(info)
+{
+    console.log(info)
+}
+function sub(num1,num2,subtraction)
+{
+    var b=num1-num2
+    console.log("subtraction of two numbers are : "+b)
+}
+function mul(num1,num2,multiplication)
+{
+    c=num1*num2
+    console.log("multiplication of two numbers are : "+c)
+}
+function div(num1,num2,division)
+{
+    var d=num1/num2
+    console.log("division of two numbers are : "+d)
+}
+
+
+
+add(10,2,output)
+sub(10,2,output)
+mul(10,2,output)
+div(10,2,output)
