ffc42c09569a7545f862167daba3e18e366e2ac7
Abhinav Choudhary <89508503+abhinavchoudhary12@users.noreply.github.com>
2022-01-13 17:51:52 +0530

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+var a=["abhinav","jaat",12]
+console.log(a)
+a.push("pushed",2)
+console.log(a)
+a.pop()
+console.log(a)
+a.sort()
+console.log(a)
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i])
+}
